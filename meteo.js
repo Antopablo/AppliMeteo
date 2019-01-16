760cb078319f62f640f3797823d3aaeec97a75e1
Antony LEFEVRE <lefevreanto57@gmail.com>
2019-01-16 11:38:06 +0100

    Météo avec design et ville modifiable

--- meteo.js
+++ meteo.js
@@ -1,3 +1,5 @@
+/*
+
 var xhr = new XMLHttpRequest();
 
 // Forme générale du lien :
@@ -63,3 +65,75 @@ function get_temperature() {
     xhr.open("GET", get_url(), true);
     xhr.send();
 }
+
+*/
+
+const weatherIcons = {
+    "Rain": "wi wi-day-rain",
+    "Clouds": "wi wi-day-cloudy",
+    "Clear": "wi wi-day-sunny",
+    "Snow": "wi wi-day-snow",
+    "mist": "wi wi-day-fog",
+    "Drizzle": "wi wi-day-sleet",
+}
+
+function capitalize(str) {
+    return str[0].toUpperCase() + str.slice(1);
+}
+
+async function main(withIP = true) {
+    let ville;
+    if (withIP) {
+
+    
+    const ip = await fetch('https://api.ipify.org?format=json')
+        .then(resultat => resultat.json())
+        .then(json => json.ip);
+
+
+
+    ville = await fetch('http://api.ipstack.com/' + ip + '?access_key=c0fa19618b5392d9c6e9fbfafae0ea9e')
+        .then(resultat => resultat.json())
+        .then(json => json.city);
+    } else 
+    ville = document.querySelector('#ville').textContent;
+
+
+    const meteo = await fetch('http://api.openweathermap.org/data/2.5/weather?q=' + ville + '&appid=4873b4305c0e97ae99f6c53a1a348ac3&lang=fr&units=metric')
+                .then(resultat => resultat.json())
+                .then(json => json)
+
+ displayWeatherInfos (meteo)
+}  
+
+function displayWeatherInfos (data) {
+    const name = data.name;
+    const temperature = data.main.temp;
+    const conditions = data.weather[0].main;
+    const description = data.weather[0].description;
+
+    document.querySelector('#ville').textContent = name;
+    document.querySelector('#temperature').textContent = temperature; // Math.round(temperature) si on veut l'arrondir
+    document.querySelector("#conditions").textContent = capitalize(description);
+    document.querySelector('i.wi').className = weatherIcons[conditions];
+
+    document.body.className = conditions.toLowerCase();
+
+    const ville = document.querySelector('#ville');
+
+    ville.addEventListener('click', () => {
+        ville.contentEditable = true;
+    });
+
+    ville.addEventListener('keydown', (e) => {
+        if(e.keyCode === 13) {  // 13 parce que le code de entré est 13
+            e.preventDefault();
+            ville.contentEditable = false;
+            main(false);
+        }
+    })
+
+}
+
+
+main();
