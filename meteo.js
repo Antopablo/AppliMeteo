f6cc0b9ba1e24115ee980675c33d440797e9878e
Antony LEFEVRE <34274907+Antopablo@users.noreply.github.com>
2019-01-20 15:11:39 +0100

    Ajout CSS + responsive design
    J'ai ajouté du CSS pour rendre la page plus jolie.
    Responsive design = OK

--- meteo.js
+++ meteo.js
@@ -1,5 +1,4 @@
 /*
-
 var xhr = new XMLHttpRequest();
 
 // Forme générale du lien :
@@ -96,7 +95,7 @@ async function main(withIP = true) {
         .then(resultat => resultat.json())
         .then(json => json.city);
     } else */
-    ville = "metz" /*document.querySelector('#ville').textContent;*/
+    ville = "tokyo" /*document.querySelector('#ville').textContent;*/
 
 
     const meteo = await fetch('http://api.openweathermap.org/data/2.5/weather?q=' + ville + '&appid=4873b4305c0e97ae99f6c53a1a348ac3&lang=fr&units=metric')
@@ -112,33 +111,72 @@ async function main(withIP = true) {
 
  
 }  
-
+//previsions
 function displayForcastInfo (data) {
-    const tempun = data.list[7].main.temp
-    const condun = data.list[7].weather[0].main
-    const tempdeux = data.list[15].main.temp
-    const conddeux = data.list[15].weather[0].main
-    const temptrois = data.list[23].main.temp
-    const condtrois = data.list[23].weather[0].main
-    const tempquatre = data.list[31].main.temp
-    const condquatre = data.list[31].weather[0].main
-    const tempcinq = data.list[39].main.temp
-    const condcinq = data.list[39].weather[0].main
+    const tempun = data.list[7].main.temp;// temperature j+1
+    const condun = data.list[7].weather[0].main;// icon j +1
+  //  const desun = data.list[7].weather[0].description;// description j+1
 
+    const tempdeux = data.list[15].main.temp;// temperature j+2
+    const conddeux = data.list[15].weather[0].main;// icon j+2
+  //  const desdeux = data.list[15].weather[0].description;// description j+2
+
+    const temptrois = data.list[23].main.temp;// temperature j+3
+    const condtrois = data.list[23].weather[0].main;// icon j+3
+   // const destrois = data.list[23].weather[0].description;// descrition j+4
+
+    const tempquatre = data.list[31].main.temp;//temperature j+4
+    const condquatre = data.list[31].weather[0].main;// icon j+4
+   // const desquatre = data.list[31].weather[0].description;// description j+4
+
+    const tempcinq = data.list[39].main.temp;//temperature j+5
+    const condcinq = data.list[39].weather[0].main;// icon j+5
+  //  const descinq = data.list[39].weather[0].description;// descrition j+5
+
+    document.querySelector('#tempun').textContent = Math.round(tempun*10)/10; // Math.round(temperature*10)/10 arrondi un chiffre derriere la virgule
+    document.querySelector('#condun').className = weatherIcons[condun];
+   // document.querySelector("#desun").textContent = capitalize(desun);
+
+    document.querySelector('#tempdeux').textContent = Math.round(tempdeux*10)/10;
+    document.querySelector('#conddeux').className = weatherIcons[conddeux];
+    //document.querySelector("#desdeux").textContent = capitalize(desdeux);
+
+    document.querySelector('#temptrois').textContent = Math.round(temptrois*10)/10;
+    document.querySelector('#condtrois').className = weatherIcons[condtrois];
+   // document.querySelector("#destrois").textContent = capitalize(destrois);
+
+    document.querySelector('#tempquatre').textContent = Math.round(tempquatre*10)/10;
+    document.querySelector('#condquatre').className = weatherIcons[condquatre];
+    //document.querySelector("#desquatre").textContent = capitalize(desquatre);
+
+    document.querySelector('#tempcinq').textContent = Math.round(tempcinq*10)/10;
+    document.querySelector('#condcinq').className = weatherIcons[condcinq];
+    //document.querySelector("#descinq").textContent = capitalize(descinq);
    
 
 
 }
 function displayWeatherInfos (data) {
     const name = data.name;
-    const temperature = data.main.temp;
-    const conditions = data.weather[0].main;
-    const description = data.weather[0].description;
+    const temperature = data.main.temp;//temperature
+    const conditions = data.weather[0].main;//icon
+    const description = data.weather[0].description;//description
+    const humidite = data.main.humidity;//taux d'humidité
+    const vitVent = data.wind.speed;//vitesse du vent
+    const pression = data.main.pressure;//pression
+    const longitude = data.coord.lon;//longitude
+    const latitude = data.coord.lat;//latitude
+    const update = data.lastupdate;
 
     document.querySelector('#ville').textContent = name;
-    document.querySelector('#temperature').textContent = temperature; // Math.round(temperature) si on veut l'arrondir
+    document.querySelector('#temperature').textContent = Math.round(temperature*10)/10; 
     document.querySelector("#conditions").textContent = capitalize(description);
     document.querySelector('i.wi').className = weatherIcons[conditions];
+    document.querySelector('#txhumide').textContent = humidite;
+    document.querySelector('#vitesse').textContent = vitVent;
+    document.querySelector('#pression').textContent = pression;
+    document.querySelector('#longitude').textContent= longitude;
+    document.querySelector('#latitude').textContent = latitude;
 
     document.body.className = conditions.toLowerCase();
 
